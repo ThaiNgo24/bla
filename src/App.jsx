ed4b254c131148586e295eeef0229605749e23f3
ThaiNgo24 <thaingo2406@gmail.com>
2026-03-11 01:23:08 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import lovesvg from "./assets/All You Need Is Love SVG Cut File.svg";
-import lovesvg2 from "./assets/Love In The Air SVG Cut File.svg";
+// import lovesvg from "./assets/All You Need Is Love SVG Cut File.svg";
+// import lovesvg2 from "./assets/Love In The Air SVG Cut File.svg";
 
 export default function Page() {
   const [noCount, setNoCount] = useState(0);
@@ -13,25 +13,25 @@ export default function Page() {
 
   const getNoButtonText = () => {
     const phrases = [
-      "No",
-      "Are you sure?",
-      "Really sure?",
-      "Think again!",
-      "Last chance!",
-      "Surely not?",
-      "You might regret this!",
-      "Give it another thought!",
-      "Are you absolutely certain?",
-      "This could be a mistake!",
-      "Have a heart!",
-      "Don't be so cold!",
-      "Change of heart?",
-      "Wouldn't you reconsider?",
-      "Is that your final answer?",
-      "You're breaking my heart ;(",
-      "Is that your final answer?",
-      "You're breaking my heart ;(",
-      "Plsss? :( You're breaking my heart",
+      "Không",
+      "Fen chắc chưa?",
+      "Thực sự luôn à fen?",
+      "Nghĩ lại đi!",
+      "Lần cuối đó!",
+      "Chắc chắn là không chứ?",
+      "Linh có thể sẽ hối hận đó!",
+      "Suy nghĩ lại chút đi mà!",
+      "Linh có hoàn toàn chắc chắn không?",
+      "Đây có thể là một sai lầm đó!",
+      "Mủi lòng chút đi mà!",
+      "Đừng lạnh lùng thế chứ!",
+      "Thay đổi ý định được không?",
+      "Linh không cân nhắc lại sao?",
+      "Đó là câu trả lời cuối cùng của Linh à?",
+      "Linh đang làm tan nát trái tim tui đó ;(",
+      "Đó là câu trả lời cuối cùng của Linh à?",
+      "Linh đang làm tan nát trái tim tui đó ;(",
+      "Đi mà? :( Linh đang làm tan nát trái tim tui",
     ];
 
     return phrases[Math.min(noCount, phrases.length - 1)];
@@ -41,27 +41,27 @@ export default function Page() {
     <div className="overflow-hidden flex flex-col items-center justify-center pt-4 h-screen -mt-16 selection:bg-rose-600 selection:text-white text-zinc-900">
       {yesPressed ? (
         <>
-          <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
+          <img src="/public/bubu-dudu-flower-gift.gif" />
           <div className="text-4xl md:text-6xl font-bold my-4">
-            Ok Yayyyyy!!!
+            Yayyy, mình cùng chơi game nhé!
           </div>
         </>
       ) : (
         <>
-          <img
+          {/* <img
             src={lovesvg}
             className="fixed animate-pulse top-10 md:left-24 left-6 md:w-40 w-28"
           />
           <img
             src={lovesvg2}
             className="fixed bottom-16 -z-10 animate-pulse md:right-24 right-10 md:w-40 w-32"
-          />
+          /> */}
           <img
             className="h-[230px] rounded-lg shadow-lg"
             src="https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.webp"
           />
           <h1 className="text-4xl md:text-6xl my-4 text-center">
-            Will you be my Valentine?
+            Linh ơi nay fen lại buồn à? Vui lên đi nhé!
           </h1>
           <div className="flex flex-wrap justify-center gap-2 items-center">
             <button
@@ -69,7 +69,7 @@ export default function Page() {
               style={{ fontSize: yesButtonSize }}
               onClick={() => setYesPressed(true)}
             >
-              Yes
+              Ok
             </button>
             <button
               onClick={handleNoClick}
@@ -92,10 +92,10 @@ const Footer = () => {
       href="https://github.com/Xeven777/valentine"
       target="__blank"
     >
-      Made with{" "}
+      {/* Made with{" "}
       <span role="img" aria-label="heart">
         ❤️
-      </span>
+      </span> */}
     </a>
   );
 };
